1339e8cd95622e5178a3b0b9755d5eb277f5a21e
Aaron Wilson <codingsensei19@gmail.com>
2020-02-01 16:03:14 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,25 +1,27 @@
-def primemr?
-     primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43]
-     return primes.include? self if self <= primes.last
-     return false unless primes.reduce(:*).gcd(self) == 1
-     wits = WITNESS_RANGES.find {|range, wits| range > self} # [range, [wit_prms]] or nil
-     witnesses = wits && wits[1] || primes
-     miller_rabin_test(witnesses)
-   end
+def prime_division(value, generator = Prime::Generator23.new)
+    raise ZeroDivisionError if value == 0
+    if value < 0
+      value = -value
+      pv = [[-1, 1]]
+    else
+      pv = []
+    end
+    generator.each do |prime|
+      count = 0
+      while (value1, mod = value.divmod(prime)
+             mod) == 0
+        value = value1
+        count += 1
+      end
+      if count != 0
+        pv.push [prime, count]
+      end
+      break if value1 <= prime
+    end
+    if value > 1
+      pv.push [value, 1]
+    end
+    pv
+  end
 
-   private
-   # Returns true if +self+ passes Miller-Rabin Test on witness +b+
-   def miller_rabin_test(witnesses)     # use witness list to test with
-     neg_one_mod = n = d = self - 1
-     d >>= 1  while d.even?
-     witnesses.each do |b|
-       s = d
-       y = b.to_bn.mod_exp(d, self)     # y = (b**d) mod self
-       until s == n || y == 1 || y == neg_one_mod
-         y = y.mod_exp(2, self)         # y = (y**2) mod self
-         s <<= 1
-       end
-       return false unless y == neg_one_mod || s.odd?
-     end
-     true
-   end
+end
