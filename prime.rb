54c75c88978a05774e560a3ff829e8b4aca77535
Aaron Wilson <codingsensei19@gmail.com>
2020-02-01 16:05:03 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,27 +1,31 @@
-def prime_division(value, generator = Prime::Generator23.new)
-    raise ZeroDivisionError if value == 0
-    if value < 0
-      value = -value
-      pv = [[-1, 1]]
-    else
-      pv = []
-    end
-    generator.each do |prime|
-      count = 0
-      while (value1, mod = value.divmod(prime)
-             mod) == 0
-        value = value1
-        count += 1
-      end
-      if count != 0
-        pv.push [prime, count]
-      end
-      break if value1 <= prime
-    end
-    if value > 1
-      pv.push [value, 1]
-    end
-    pv
+def sieve(max)
+  # Set up an array with all the numbers from 0 to the max
+  primes = (0..max).to_a
+
+  # Set both the first and second positions (i.e., 0 and 1) to nil, as they
+  # aren't prime.
+  primes[0] = primes[1] = nil
+
+  # Iterate through primes array
+  counter = 0
+  primes.each do |p|
+    # Skip if nil
+    next unless p
+
+    # Break if we are past the square root of the max value 
+    break if p*p > max
+    counter += 1
+    # Start at the square of the current number, and step through.
+    # Go up to the max value, by multiples of the current number, and replace
+    # that value with nil in the primes array
+    (p*p).step(max,p) { |m| primes[m] = nil }
   end
 
+  # Finally, return the compacted array.
+  puts "Solved for #{max} in #{counter} steps."
+  primes.compact
+end
+
+def prime?(num)
+  sieve(num).include?(num)
 end
