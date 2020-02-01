047b3071a87a4cd5d6353ed2fd09fdc750e895ba
Aaron Wilson <codingsensei19@gmail.com>
2020-02-01 15:54:07 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,18 +1,25 @@
-def prime?
-  return self >= 2 if self <= 3
-  return true if self == 5
-  return false unless 30.gcd(self) == 1
-  (7..Integer.sqrt(self)).step(30) do |p|
-    return false if
-      self%(p)    == 0 || self%(p+4)  == 0 || self%(p+6)  == 0 || self%(p+10) == 0 ||
-      self%(p+12) == 0 || self%(p+16) == 0 || self%(p+22) == 0 || self%(p+24) == 0
-  end
-  true
-end
+def primemr?
+     primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43]
+     return primes.include? self if self <= primes.last
+     return false unless primes.reduce(:*).gcd(self) == 1
+     wits = WITNESS_RANGES.find {|range, wits| range > self} # [range, [wit_prms]] or nil
+     witnesses = wits && wits[1] || primes
+     miller_rabin_test(witnesses)
+   end
 
-# Iterates the given block over all prime numbers.
-# See +Prime+#each for more details.
-def Integer.each_prime(ubound, &block) # :yields: prime
-  Prime.each(ubound, &block)
-end
-end
+   private
+   # Returns true if +self+ passes Miller-Rabin Test on witness +b+
+   def miller_rabin_test(witnesses)     # use witness list to test with
+     neg_one_mod = n = d = self - 1
+     d >>= 1  while d.even?
+     witnesses.each do |b|
+       s = d
+       y = b.to_bn.mod_exp(d, self)     # y = (b**d) mod self
+       until s == n || y == 1 || y == neg_one_mod
+         y = y.mod_exp(2, self)         # y = (y**2) mod self
+         s <<= 1
+       end
+       return false unless y == neg_one_mod || s.odd?
+     end
+     true
+   end
